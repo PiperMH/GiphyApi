713554cd78e445fa624153a376c716185ad5b3a8
PiperMH <piper.1.hayden@gmail.com>
2017-09-05 12:25:19 -0500

    and again

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,5 +1,5 @@
 $(function(){
-	populateButtons(searchArray, "searchButton", "#buttonsArea");
+	populateButtons(searchArray,"searchButton","#buttonsArea");
 	console.log("ahh yeah");
 })
 
@@ -14,14 +14,20 @@ function populateButtons(searchArray,classToAdd,areaToAddTo){
 		b.text(searchArray[i]);
 		$(areaToAddTo).append(b);
 	}
-}
+};
 
-$(document).on("click", ".searchButton", function(){
+$(document).on("click",".searchButton", function(){
+	$('#searches').empty();
 	var type = $(this).data("type");
-	 var queryURL = "http://api.giphy.com/v1/gifs/search?q="+type+"&api_key=f9807de676eb433e8a7b4d9a214a3041&limit=10";
-	$.ajax({url:queryURL,method:'GET'})
-		.done(function(response){
-			for(var i = 0; i < response.data.length[i]; i++){
+	console.log(type);
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+type+"&api_key=f9807de676eb433e8a7b4d9a214a3041&limit=10";
+	
+	$.ajax({
+		url:queryURL,
+		method:'GET'
+	}).done(function(response){
+			console.log(response);
+			for(var i = 0; i < response.data.length; i++){
 				var searchDiv = $("<div class='search-item'>");
 				var rating = response.data[i].rating;
 				var p = $("<p>").text("Rating: " + rating);
@@ -30,8 +36,8 @@ $(document).on("click", ".searchButton", function(){
 				var image = $("<img>");
 				image.attr('src',still);
 				image.attr('data-still',still);
-				image.attr('data-animated', animated);
-				image.attr('data-state', 'still');
+				image.attr('data-animated',animated);
+				image.attr('data-state','still');
 				image.addClass('searchImage');
 				searchDiv.append(p);
 				searchDiv.append(image);
@@ -42,7 +48,7 @@ $(document).on("click", ".searchButton", function(){
 
 $(document).on("click",".searchImage", function(){
 	var state = $(this).attr("data-state");
-	if( state == still){
+	if(state == 'still'){
 		$(this).attr("src", $(this).data('animated'));
 		$(this).attr("data-state", "animated");
 	}else{
@@ -51,10 +57,11 @@ $(document).on("click",".searchImage", function(){
 	}
 })
 
-$("#addSearch").on("click",function(){
+
+/*$("#addSearch").on("click",function(){
 	var newSearch = $("input").eq(0).val();
 	searchArray.push(newSearch);
 	populateButtons(searchArray,"searchButton", "#buttonsArea");
-	return false;
+	return false; })
 
-})
+*/
